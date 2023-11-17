83f859b3233931226dcebe8b695308b5e1ecbbd5
Henry Saya <henrysaya@skiff.com>
2023-11-17 15:48:25 +0300

    simple shell .1

--- shell.c
+++ shell.c
@@ -8,12 +8,20 @@
 int main(void)
 {
 	char *cmd;
+	char *token;
 
 	while (true)
 	{
 		promptDisplay();
 		cmd = read_cmd();
-		execute(cmd);
+
+		token = strtok(cmd, " ");
+
+		while (token != NULL)
+		{
+			execute(token);
+			token = strtok(NULL, " ");
+		}
 
 		free_cmd(cmd);
 	}
